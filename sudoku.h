a70c2fe0b48a5fde65a34b353cf7b45088fc94a3
aditya1904 <maluaditya12@gmail.com>
2015-11-15 16:34:53 +0530

    Sudoku Done.

--- sudoku.h
+++ sudoku.h
@@ -8,5 +8,4 @@ void evil(int **grid, int holes);
 void exteasy(int **grid);
 void medium(int **grid, int holes);
 int check(int **grid, int row, int col, int num);
-//void display(int **grid);
-void play(int **grid, int **solutiongrid);
+
